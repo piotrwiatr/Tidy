76af6a8e8f2b4dd87e3cad9d2229eaf68f912a3d
piotrwiatr <peterwiatr6235@gmail.com>
2023-02-04 12:14:30 -0500

    stored key as env var, added forms, styling

--- Tidy/main/static/main/js/script.js
+++ Tidy/main/static/main/js/script.js
@@ -1,8 +1,18 @@
+const categories = ["laptop", "desktop", "camera"]
+
+// shows one page and hides the other two
+function showPage(page) {
+    for (let i = 0; i < categories.length; i++) {
+        document.querySelector(`#${categories[i]}`).style.display = "none";
+    }
+    document.querySelector(`#${page}`).style.display = "block";
+}
+
 document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector("form").onsubmit = function() {
-        const categoryForm = document.querySelector("#categories");
-        const index = categoryForm.selectedIndex;
-        const category = categoryForm[index].value;
-        alert(`${category}`);
-    };
+    for (let i =0; i < categories.length; i++) {
+        document.querySelector(`#${categories[i]}-btn`).onclick = function() {
+            page = this.dataset.page;
+            showPage(page);
+        }
+    }
 });
